bcded3c556c29040f2c7fd7d2374c694744b72b4
Carl Alsford <carl@alsford.me.uk>
2024-06-30 14:00:46 +0100

    Documentation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # WordFrequencyAnalyzer
+
+A library used to determine the frequency that words appear in a given input.
+Features include finding highest frequency word, the frequency for any given word, and the 'n' most frequent words for any given text input.
+
+## Usage
+
+```c#
+public int GetHighestFrequency(string text)
+{
+    var analyzer = new WordFrequencyAnalyzer();
+
+    return analyzer.CalculateHighestFrequency(text);
+}
--- WordFrequencyAnalyzer/Interfaces/IWordFrequencyAnalyzer.cs
+++ WordFrequencyAnalyzer/Interfaces/IWordFrequencyAnalyzer.cs
@@ -7,6 +7,8 @@ public interface IWordFrequencyAnalyzer
     /// </summary>
     /// <param name="text">The string of string you are testing against</param>
     /// <returns>How often the most frequent word appeared</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="text"/> is invalid (i.e., contains anything except a-z and space).</exception>
     int CalculateHighestFrequency(string text);
 
     /// <summary>
@@ -15,6 +17,9 @@ public interface IWordFrequencyAnalyzer
     /// <param name="text">The string of string you are testing against</param>
     /// <param name="word">The word you wish to count the frequency of</param>
     /// <returns>How often the word provided was found in the text provided</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> or <paramref name="word"/> are null</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="text"/> is invalid (i.e., contains anything except a-z and space).</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="word"/> is invalid (i.e., contains anything except a-z).</exception>
     int CalculateFrequencyForWord(string text, string word);
 
     /// <summary>
@@ -23,5 +28,8 @@ public interface IWordFrequencyAnalyzer
     /// <param name="text">The string of string you are testing against</param>
     /// <param name="number">How many words you wish to return</param>
     /// <returns>A list of the implementation of the IWordFrequency interface of the top most common words</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="text"/> is null</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="text"/> is invalid (i.e., contains anything except a-z and space).</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="number"/> is invalid (i.e., is less than 1).</exception>
     IList<IWordFrequency> CalculateMostFrequentWords(string text, int number);
 }
